3811a5f3500311ee77253ec5be34d5208dd8de7a
Tej Lehal <tejlehal13@gmail.com>
2024-11-20 16:36:23 -0500

    Added edit Form

--- app/components/EditForm.tsx
+++ app/components/EditForm.tsx
@@ -1,5 +1,76 @@
-const EditForm = () => {
-  return <div>EditForm</div>
+const EditForm = ({ formData, handleInputChange, handleSubmit }) => {
+  return (
+    <div>
+      <h2 className="text-xl text-green-teal font-semibold mb-4">
+        Add Job Application
+      </h2>
+      <form onSubmit={handleSubmit} className="space-y-4">
+        <div>
+          <label className="block text-sm font-medium text-gray-700">
+            Company
+          </label>
+          <input
+            type="text"
+            name="company"
+            value={formData.company}
+            onChange={handleInputChange}
+            className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
+            required
+          />
+        </div>
+        <div>
+          <label className="block text-sm font-medium text-gray-700">
+            Position
+          </label>
+          <input
+            type="text"
+            name="position"
+            value={formData.position}
+            onChange={handleInputChange}
+            className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
+            required
+          />
+        </div>
+        <div>
+          <label className="block text-sm font-medium text-gray-700">
+            Date Applied
+          </label>
+          <input
+            type="date"
+            name="dateApplied"
+            value={formData.dateApplied}
+            onChange={handleInputChange}
+            className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
+            required
+          />
+        </div>
+        <div>
+          <label className="block text-sm font-medium text-gray-700">
+            Status
+          </label>
+          <select
+            name="status"
+            value={formData.status}
+            onChange={handleInputChange}
+            className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
+            required
+          >
+            <option value="">Select Status</option>
+            <option value="Applied">Applied</option>
+            <option value="Interviewing">Interviewing</option>
+            <option value="Offer">Offer</option>
+            <option value="Rejected">Rejected</option>
+          </select>
+        </div>
+        <button
+          type="submit"
+          className="w-full py-2 px-4 bg-green-teal text-clicked-black rounded-md shadow-sm hover:bg-green-600 focus:outline-none"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  )
 }
 
 export default EditForm
